da9d8fa745774152e9c0adc0873881bd9fab4e47
jake-the-cake <askjake331@gmail.com>
2022-07-05 14:18:13 -0400

    make viewable

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const pageContent = document.getElementById('root');
+const pageTitle = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch('../src/components/title.html')
+        .then(response => {
+        return response.text();
+    });
+    const html = String(res);
+    return html;
+});
+const app = (() => __awaiter(void 0, void 0, void 0, function* () {
+    pageContent.innerHTML = (yield pageTitle()).valueOf();
+}))();
